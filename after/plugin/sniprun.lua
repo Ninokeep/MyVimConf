123659d8a10abd48ff1333efa0935b621e82f04f
fababy <studev170@gmail.com>
2023-01-28 06:32:13 +0100

    remove the file sniprun

--- after/plugin/sniprun.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_set_keymap('v', 't', '<Plug>SnipRun', {silent = false})
-vim.api.nvim_set_keymap('n', '<leader>ru', '<Plug>SnipRunOperator', {silent = true})
-vim.api.nvim_set_keymap('n', '<leader>tt', '<Plug>SnipRun', {silent = true})
